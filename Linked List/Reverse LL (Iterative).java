73ab289fc22a4055f9fff047ba2769b4fc7fc8b8
siddhantgupta547 <sidgpt84@gmail.com>
2021-12-17 23:49:23 +0530

    Reverse LL (Iterative)

---
+++ Linked List/Reverse LL (Iterative).java
@@ -0,0 +1,77 @@
+/* Reverse LL (Iterative)
+Question
+Given a singly linked list of integers, reverse it iteratively and return the head to the modified list.
+ Note :
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a separate line.
+ Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^4
+Where N is the size of the singly linked list.
+
+Time Limit: 1 sec
+Sample Input 1 :
+1
+1 2 3 4 5 6 7 8 -1
+Sample Output 1 :
+8 7 6 5 4 3 2 1
+Sample Input 2 :
+2
+10 -1
+10 20 30 40 50 -1
+Sample Output 2 :
+10 
+50 40 30 20 10 
+*/
+
+
+/*Solution
+class LinkedListNode<T> {
+	public T data;
+	public LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.setData(data);
+		this.next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+}
+* */
+public class Solution {
+	
+	public static LinkedListNode<Integer> reverse_I(LinkedListNode<Integer> head) {
+    	if(head==null || head.next==null)
+            return head;
+        LinkedListNode<Integer> temp1= head.next;
+        LinkedListNode<Integer> temp2= head;
+        head.next=null;
+        head=temp1;
+        temp1=temp1.next;
+        head.next=temp2;
+        temp2=head;
+        while(temp1!=null){
+        	head=temp1;
+            temp1=temp1.next;
+        	head.next=temp2;
+        	temp2=head;
+        }
+        return head;
+	}
+}
